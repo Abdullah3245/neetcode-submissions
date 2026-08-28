d755d9977eb682e92a7e0d651a518489a58c4350
Abdullah Goher <137772368+Abdullah3245@users.noreply.github.com>
2026-08-28 10:10:53 -0400

    Add: minimum-stack - submission-3

---
+++ Data Structures & Algorithms/minimum-stack/submission-3.py
@@ -0,0 +1,23 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = deque()
+        self.min_val = deque()
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if not self.min_val or self.min_val[-1] > val:
+            self.min_val.append(val)
+        else:
+            self.min_val.append(self.min_val[-1])
+
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.min_val.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_val[-1]
