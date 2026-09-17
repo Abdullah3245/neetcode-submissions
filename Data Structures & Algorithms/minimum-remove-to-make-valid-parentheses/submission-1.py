e9b976d33d92c5561e908d504e6594a3cf676fd6
Abdullah Goher <137772368+Abdullah3245@users.noreply.github.com>
2026-09-17 09:20:11 -0400

    Add: minimum-remove-to-make-valid-parentheses - submission-1

---
+++ Data Structures & Algorithms/minimum-remove-to-make-valid-parentheses/submission-1.py
@@ -0,0 +1,30 @@
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        stack = []
+        closing = 0
+        opening = 0
+        for c in s:
+            if c == "(":
+                stack.append(c)
+                opening += 1
+            elif c.isalpha():
+                stack.append(c)
+            if c == ")" and opening > closing:
+                stack.append(c)
+                closing += 1
+        
+        s = ""
+        opening = 0
+        i = 0
+        while i < len(stack):
+            if stack[i].isalpha():
+                s += stack[i]
+            elif stack[i] == ")":
+                s += stack[i]
+            elif stack[i] == "(" and closing > opening:
+                opening += 1
+                s += stack[i]
+            i += 1
+        return s
+        
+            
