1b184ab8c6e18d7d1bb89e715135e25c7d38408b
Abdullah Goher <137772368+Abdullah3245@users.noreply.github.com>
2026-09-19 10:21:39 -0400

    Add: rearrange-array-elements-by-sign - submission-0

---
+++ Data Structures & Algorithms/rearrange-array-elements-by-sign/submission-0.py
@@ -0,0 +1,29 @@
+class Solution:
+    def rearrangeArray(self, nums: List[int]) -> List[int]:
+        res = []
+
+        positive, negative = 0, 0
+
+        def next_pos():
+            nonlocal positive
+            while nums[positive] < 0 and positive < len(nums):
+                positive += 1
+        
+        def next_neg():
+            nonlocal negative
+            while nums[negative] > 0 and negative < len(nums):
+                negative += 1
+
+        while positive < len(nums) and negative < len(nums):
+            next_pos()
+            next_neg()
+            print(f"Positive = {positive}")
+            print(f"negative = {negative}")
+            if positive >= len(nums) or negative >= len(nums):
+                break
+            res.append(nums[positive])
+            res.append(nums[negative])
+            positive += 1
+            negative += 1
+        
+        return res
